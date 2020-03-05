bc711098288b3a1d6b6562682d6e47919017f82c
Timothy Onyiuke <timothyonyiuke@gmail.com>
2020-03-05 04:23:19 +0100

    first release 🚀

--- README.md
+++ README.md
@@ -1,19 +1,51 @@
-# joncalhoun-dl
+# joncalhoun-dl 🔥⬇
 
-> **Before you proceed, note that you must be a paid subscriber for this to work**
+Downloads Go tutorial videos from joncalhoun.io
 
-Downloads videos from joncalhoun.io
+> **Before you proceed, note that you must be a paid user for the paid content to download**
+
+Kindly create your account [here](https://courses.calhoun.io/signup?). Jon is a great teacher, consider buying his premium courses if want to.
 
 ## How to use
 
-Still under develooment, I haven't really fleshed out how this is going to work.
++ Clone this repo and then run the command below
 
 ```bash
-    $ go run main.go --email=jon@doe.com --password=12345 -course=testwithgo
+    $ git clone git@github.com:timolinn/joncalhoun-dl.git
+    $ cd joncalhoun-dl
+    $ go run main.go --email=jon@doe.com --password=12345 --course=gophercises
+     [joncalhoun.io]: fetching video urls for gophercises
+     [joncalhoun.io]: fetching data from https://courses.calhoun.io/courses/cor_gophercises...
 ```
+
+### Command [options]
+
++ `--email` : Your account email address. Sign up [here](https://courses.calhoun.io/signup?)
++ `--password` : Your account password. _Unlike the unix password prompt, this will not hide your password by default, you'll have to keep an eye over your shoulder 😉_
++ `--course` : This is the name of the course you want to download. **Defaults to `"gophercises"`**
 
 ### Supported courses
 
-- [ ] [Test With Go](https://courses.calhoun.io/courses/cor_test)
-- [ ] [Algorithms](https://courses.calhoun.io/courses/cor_algo)
-- [ ] [Gophercises](https://courses.calhoun.io/courses/cor_gophercises)
++ [x] [testwithgo](https://courses.calhoun.io/courses/cor_test)
++ [x] [gophercises](https://courses.calhoun.io/courses/cor_gophercises)
++ [ ] [algorithms](https://courses.calhoun.io/courses/cor_algo)
+
+### Contributing
+
+There is still a couple features to implement, check the TODO list below and send a pull request.
+
+## TODO
+
++ [x] Add caching for deterministic requests
++ [x] Add default output directory
++ [ ] Add Packaged release and semver
++ [ ] check for authentication error
++ [ ] Add output directoy flag
++ [ ] prevent signin when using cache
++ [ ] choose video quality
+
+### Authors
+
++ Timothy Onyiuke _([twitter](https://twitter.com/timolinn_))_
+
+Remeber to Star this repo if you like it! 🔥
--- main.go
+++ main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
 	"os"
+	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/publicsuffix"
 )
 
 var email = flag.String("email", "", "your email")
 var password = flag.String("password", "", "your password")
-var course = flag.String("course", "testwithgo", "course name")
+var course = flag.String("course", "gophercises", "course name")
 
 // this will be used by youtube-dl binary to download video
 var referer = "https://courses.calhoun.io"
@@ -26,7 +30,9 @@ var referer = "https://courses.calhoun.io"
 var courses = map[string]string{
 	"testwithgo":  "https://courses.calhoun.io/courses/cor_test",
 	"gophercises": "https://courses.calhoun.io/courses/cor_gophercises",
+	"algorithms":  "https://courses.calhoun.io/courses/cor_algo",
 }
+var delayDuration = 5
 
 func checkError(err error) {
 	if err != nil {
@@ -35,74 +41,181 @@ func checkError(err error) {
 }
 
 func main() {
-	// Parse commanline options
+	// Parse commandline options
 	flag.Parse()
 
 	// Cookiejar provides automatic cookie management
-	// that would normally be aaccessed only via the browser
-	// options := cookiejar.Options{
-	// 	PublicSuffixList: publicsuffix.List,
-	// }
-	// jar, err := cookiejar.New(&options)
-	// checkError(err)
+	// that would normally be accessed only via the browser
+	options := cookiejar.Options{
+		PublicSuffixList: publicsuffix.List,
+	}
+	jar, err := cookiejar.New(&options)
+	checkError(err)
 
-	// client := &http.Client{Jar: jar}
+	client := &http.Client{Jar: jar}
 
 	// Login
-	// res, client := signin(client)
+	signin(client)
 
-	// Visit selected course page and fetch video paths
-	// There are currently 4 different path pattern
-	// `/les_twg_les_01` => for tESTwITHgO tests videos
-	// `/les_form_les_01` => for tESTwITHgO form project videos
-	// `/les_stripe_les_01` => for tESTwITHgO stripe project videos
-	// `/les_swag_les_01` => for tESTwITHgO swag project videos
-	// res, err := client.Get(courses[*course])
-	// checkError(err)
-	getURLs()
+	// Visit selected course page and fetch video urls
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	videoURLs := getURLs(client)
+	for i, videoURL := range videoURLs {
+		fmt.Printf("[joncalhoun.io]: downloading lesson 0%d via %s\n", i+1, videoURL)
+		fmt.Printf("[exec]: youtube-dl %s --referer %s\n", videoURL, referer)
+		cmd := exec.CommandContext(ctx, "youtube-dl", videoURL, "--referer", referer, "-o",
+			"./videos/"+*course+"/%(title)s.%(ext)s")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+		if err := cmd.Wait(); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("[joncalhoun.io]: downloaded lesson 0%d\n", i+1)
+	}
+	fmt.Println("Done! 🚀")
 }
 
-func signin(client *http.Client) (*http.Response, *http.Client) {
+func signin(client *http.Client) {
 	// Login and create session
 	if *email == "" || *password == "" {
 		log.Fatal(errors.New("[Error] try: 'go run main.go --email=jon@examp.com --password=12345'"))
 	}
 
-	res, err := client.PostForm("https://courses.calhoun.io/signin", url.Values{
+	fmt.Println("[joncalhoun.io]: signing in...")
+	_, err := client.PostForm("https://courses.calhoun.io/signin", url.Values{
 		"email":    {*email},
 		"password": {*password},
 	})
 	checkError(err)
-
-	body, err := ioutil.ReadAll(res.Body)
-	checkError(err)
-	res.Body.Close()
-	fmt.Println(string(body))
-	return res, client
+	fmt.Println("[joncalhoun.io]: sign in successful")
 }
 
-func getCourseHTML(name string, res io.Reader) {
-	f, err := os.Create(name)
+func getCourseHTML(url string, client *http.Client) {
+	// Make a Get Request to the course URL
+	// user must be logged in
+	fmt.Printf("[joncalhoun.io]: fetching data for %s...\n", url)
+	res, err := client.Get(url)
 	checkError(err)
-	defer f.Close()
-	writer := bufio.NewWriter(f)
-	num, err := writer.ReadFrom(res)
-	checkError(err)
-	fmt.Printf("Wrote %d bytes", num)
+	defer res.Body.Close()
 
-	writer.Flush()
+	// Write raw data to file
+	saveHTMLContent(*course+".html", res.Body)
 }
 
-func getURLs() []string {
+func getURLs(client *http.Client) []string {
+	fmt.Printf("[joncalhoun.io]: fetching video urls for %s\n", *course)
 	var urls []string
-	f, err := os.OpenFile("testwithgo.html", os.O_RDWR, 066)
-	doc, err := goquery.NewDocumentFromReader(f)
+	var file *os.File
+	var err error
+
+	// check if course page is cached
+	if fileExists(*course + ".html") {
+		fmt.Printf("[joncalhoun.io]: loading course page from cache: %s.html\n", *course)
+		file, err = os.OpenFile(*course+".html", os.O_RDWR, 0666)
+		checkError(err)
+	} else {
+		// fecth from remote if not cached
+		res, err := client.Get(courses[*course])
+		checkError(err)
+		defer res.Body.Close()
+
+		// cache raw HTML data
+		getCourseHTML(courses[*course], client)
+		file, err = os.OpenFile(*course+".html", os.O_RDWR, 0666)
+		checkError(err)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(file)
 	checkError(err)
+
+	// parses the HTML tree to extract videos pages
 	doc.Find("a").Each(func(i int, s *goquery.Selection) {
 		href, _ := s.Attr("href")
-		if strings.Contains(href, "/lessons/les_twg") {
-			urls = append(urls, "https://courses.calhoun.io"+href)
+		switch *course {
+		case "testwithgo":
+			if strings.Contains(href, "/lessons/les_twg") {
+				urls = append(urls, "https://courses.calhoun.io"+href)
+			}
+		case "gophercises":
+			if strings.Contains(href, "/lessons/les_goph") {
+				urls = append(urls, "https://courses.calhoun.io"+href)
+			}
+		case "algorithms":
+			log.Fatal("Algorithms not supported yet")
+		default:
+			log.Fatal("course not supported yet. feel free to send a pull request")
 		}
 	})
-	return urls
+
+	videoURLs := []string{}
+	for _, url := range urls {
+		videoURLs = append(videoURLs, getVideoURL(url, client))
+		fmt.Printf("[joncalhoun.io]: waiting 5 seconds\n")
+		time.Sleep(time.Duration(delayDuration) * time.Second)
+	}
+	fmt.Println("Nothing here")
+	return videoURLs
+}
+
+func getVideoURL(url string, client *http.Client) string {
+	fmt.Printf("[joncalhoun.io]: fetching video url for lesson %s\n", url)
+	var videoID string
+	var file *os.File
+	var err error
+
+	// check cache for existing webpage
+	name := strings.Split(url, "/")[4]
+	filename := "webpage/" + name + ".html"
+	if fileExists(filename) {
+		fmt.Printf("[joncalhoun.io]: loading cache from %s\n", filename)
+		file, err = os.OpenFile(filename, os.O_RDWR, 0666)
+		checkError(err)
+
+		// no need to delay when loading from cash
+		delayDuration = 0
+	} else {
+		// fetch web page where video lives
+		res, err := client.Get(url)
+		checkError(err)
+		defer res.Body.Close()
+
+		// To provide caching support we save the resulting
+		// html in the webpage folder
+		saveHTMLContent(filename, res.Body)
+		file, err = os.OpenFile(filename, os.O_RDWR, 0666)
+		checkError(err)
+		delayDuration = 5
+	}
+
+	// convert return data to parsable HTML Document
+	doc, err := goquery.NewDocumentFromReader(file)
+	checkError(err)
+	iframe := doc.Find("iframe")
+	videoID, _ = iframe.Attr("src")
+	fmt.Printf("[joncalhoun.io]:[video ID] %s\n", videoID)
+	return videoID
+}
+
+func saveHTMLContent(filename string, r io.Reader) {
+	f, err := os.Create(filename)
+	checkError(err)
+	defer f.Close()
+	filewriter := bufio.NewWriter(f)
+	_, err = filewriter.ReadFrom(r)
+	checkError(err)
+	fmt.Printf("[joncalhoun.io]: web page data written to %s\n", filename)
+
+	filewriter.Flush()
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
